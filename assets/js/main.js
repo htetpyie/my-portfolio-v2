41f76881f98809351bca9feaffab114af229fbe1
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-10-19 21:49:48 +0630

    feat : projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -240,23 +240,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
-
-// Back to
-$(document).ready(() => {
-  
-  const backToTop = $('#backToTop')
-  const amountScrolled = 300
-  
-  $(window).scroll(() => {
-    $(window).scrollTop() >= amountScrolled 
-      ? backToTop.fadeIn('fast') 
-      : backToTop.fadeOut('fast')
-  })
-  
-  backToTop.click(() => {
-    $('body, html').animate({
-      scrollTop: 0
-    }, 600)
-    return false
-  })
-})
